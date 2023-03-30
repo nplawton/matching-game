7045800b2daeb6c353fd3456b45f4986f4973f92
nplawton <nplawton02@gmail.com>
2023-03-30 16:18:38 -0700

    database information

---
+++ server/migrate.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const pool = require('./dbConn');
+
+pool.query(`DROP TABLE IF EXISTS moncard`)
+    .then((data) => {
+        console.log('Tables Dropped!');
+        return pool.query(`CREATE TABLE IF NOT EXISTS moncard (
+            id SERIAL PRIMARY KEY NOT NULL,
+            mon_name VARCHAR(200) NOT NULL,
+            mon_img text,
+            descrip text           
+        )`);
+    })
+.then((data) => {
+    console.log('monCard created!');
+})
+.catch((error) => {
+    console.log('Error:', error)
+})
---
+++ server/server.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//Establish all dependicies for the project
+const { Pool } = require('pg');
+const ex = require ('express');
+const cors = require('cors');
+const body = require('body-parser');
+
+//port that express will listean to for requests
+const port = process.env.port || 8000;
+
+//using dependcies
+const app = ex();
+app.use(body.json());
+app.use(cors());
+
+const pool = require('./dbConn');
+
+app.get('/moncard', (req, res, next) => {
+    pool.query('SELECT * FROM moncard', (err, results) => {
+        if (err){
+            return next(err);
+        }
+
+        let row = results.rows;
+        console.log(row);
+        res.send(row);
+
+    });
+});
+
+//Port Listeaning
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+    console.log('connecting to postgres pool:', pool);
+});
+
+
+app.use((err, req, res, next) => {
+    //console.error('We\'re not here right now');
+    console.error(err.slack);
+    req.status(400).send('We\'re not here right now');
+});
+
+module.exports = app;

891c092057a6170f027763ce2805743ced0e4999
nplawton <nplawton02@gmail.com>
2023-03-30 17:16:02 -0700

    initial fetch received

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import Recat, { useState, useEffect } from 'react';
 
 function App(){
-  return (
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('https://react-mvp-1ll9.onrender.com/moncard').then(
+      response => response.json()
+    ).then(
+      data =>{
+        setBackendData(data);
+      }
+    )
+  }, [])
+
+  return(
     <div>
 
     </div>
-  );
+  )
+
 }
 
 export default App;

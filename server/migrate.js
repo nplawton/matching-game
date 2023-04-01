f79a10184e7d68be15b7d85aab7c05cee28f3c74
nplawton <nplawton02@gmail.com>
2023-03-31 17:56:51 -0700

    updated for game1

--- server/migrate.js
+++ server/migrate.js
@@ -10,8 +10,8 @@ pool.query(`DROP TABLE IF EXISTS moncard`)
             mon_name VARCHAR(200) NOT NULL,
             mon_img text,
             descrip text,
-            matchFound BOOLEAN NOT NULL,
-            flipped BOOLEAN NOT NULL           
+            matchFound BOOLEAN,
+            flipped BOOLEAN         
         )`);
     })
 .then((data) => {
